335ef8d320cab72e232ea97642ba07e42f985fbd
Diana Celine <133211618+dianaceline376@users.noreply.github.com>
2024-03-08 15:33:27 +0300

    Add files via upload

---
+++ loan.py
@@ -0,0 +1,18 @@
+#logical operators
+#prompt tthe user to enter age
+
+age=int(input("Enter your age:"))
+
+#prompt the user to enter the income
+
+income=int(input("Enter your income:"))
+
+# testing the conditions using if else statements
+
+if age>=21 and income>=21000:
+     print("CONGRATULATIONS YOU ARE QUALIFIED FOR A LOAN")
+     
+else:
+    print("UNFOUTINATELY,WE ARE UNABLE TO OFFER YOU A LOAN AT THIS TIME")
+     
+
